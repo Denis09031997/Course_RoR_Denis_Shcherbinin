ebc2808dff124dd0a43e626e764b6c90502ead7b
Denis <den.scherbinin2018@yandex.ru>
2023-12-06 07:33:01 +0500

    Lesson 5, version 2

--- Lesson_5/company_manufacturer.rb
+++ Lesson_5/company_manufacturer.rb
@@ -1,12 +1,3 @@
 module CompanyManufacturer
   attr_accessor :manufacturer
-
-  def set_manufacturer(manufacturer)
-    self.manufacturer = manufacturer
-  end
-
-  def get_manufacturer
-    self.manufacturer
-  end
-
 end
